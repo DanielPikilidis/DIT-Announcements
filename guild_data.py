66188a08d4d193c92c3238888eff4d25defab621
DanielPikilidis <danielpikilidis@mail.com>
2022-03-04 20:43:59 +0200

    Addition to previous commit

--- guild_data.py
+++ guild_data.py
@@ -5,43 +5,43 @@ class GuildData:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         try:
-            with open("data/guilds.json", "r") as file:
+            with open("data/data.json", "r") as file:
                 self.data = loads(file.read())
                 self.backup = self.data.copy()
         except IOError:
             return None
 
     def get_announcement_channels(self):
-        arr = list(self.data.values())
+        arr = list(self.data["guilds"].values())
         for i in range(len(arr)):
             arr[i] = arr[i]["announcements"]
 
         return arr
 
     def set_announcements_channel(self, guild, channel):
-        self.data[guild]["announcements"] = channel
+        self.data["guilds"][guild]["announcements"] = channel
         return self.write_to_json()
 
     def add_control(self, guild, role):
         try:
-            self.data[guild]["control"].remove(role)
-            self.data[guild]["control"].append(role)
+            self.data["guilds"][guild]["control"].remove(role)
+            self.data["guilds"][guild]["control"].append(role)
             return 0
         except:
             pass
 
-        self.data[guild]["control"].append(role)
+        self.data["guilds"][guild]["control"].append(role)
         return self.write_to_json()
 
     def remove_control(self, guild, role):
         try:
-            self.data[guild]["control"].remove(role)
+            self.data["guilds"][guild]["control"].remove(role)
         except:
             return 0
         return self.write_to_json()
 
     def get_control_list(self, guild):
-        return self.data[guild]["control"]
+        return self.data["guilds"][guild]["control"]
 
     def has_permission(self, ctx: commands.Context):
         if ctx.author == ctx.guild.owner:
@@ -58,23 +58,23 @@ class GuildData:
                         return True
 
     def add_guild(self, guild, channel):
-        self.data[guild] = {"announcements": channel, "control": []}
+        self.data["guilds"][guild] = {"announcements": channel, "control": []}
         return self.write_to_json()
 
     def remove_guild(self, guild):
-        self.data.pop(guild)
+        self.data["guilds"].pop(guild)
         return self.write_to_json()
 
     def write_to_json(self):
         try:
-            with open("data/guilds.json", "w") as f:     
+            with open("data/data.json", "w") as f:     
                 dump(self.data, f, indent=4)
             self.backup = self.data.copy()
             return 1
         except:
             self.bot.logger.error("Failed to make changes to json file. Reverting to old.")
             self.data = self.backup.copy()
-            with open("data/guilds.json", "w") as f:     
+            with open("data/data.json", "w") as f:     
                 dump(self.data, f, indent=4)
             return 0
 
@@ -84,13 +84,13 @@ class GuildData:
 
         joined = self.bot.guilds
         try:
-            with open("data/guilds.json", "r") as f:
+            with open("data/data.json", "r") as f:
                 cur_data = loads(f.read())
         except IOError:
-            self.bot.logger.critical("data/guilds.json doesn't exist.")
+            self.bot.logger.critical("data/data.json doesn't exist.")
             return 0
 
-        stored = list(cur_data.keys())
+        stored = list(cur_data["guilds"].keys())
 
         for i in range(len(joined)):
             joined[i] = str(joined[i].id)
@@ -112,10 +112,10 @@ class GuildData:
                 else:
                     self.bot.logger.warning(f"Guild {guild.id}: Failed to add to json file.")
 
-        with open("data/guilds.json", "r") as f:
+        with open("data/data.json", "r") as f:
             cur_data = loads(f.read())
         
-        stored = list(cur_data.keys())
+        stored = list(cur_data["guilds"].keys())
 
         if joined == stored:
             return 1
